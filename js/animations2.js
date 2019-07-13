60457c8c3a22d32b6999b75e88bb82f1f58f3005
liorbey <lior.bey95@gmail.com>
2019-07-13 09:58:48 -0400

    oldchanges

--- js/animations2.js
+++ js/animations2.js
@@ -9,3 +9,14 @@ $(document).ready(function() {
     });
   
   });
+
+$(function(){
+  $('#giflist li a img').hover(function(){
+    // on mouse enter
+    var customdata = $(this).parent().attr('href');
+    $(this).attr('src',customdata); 
+  }, function(){
+    // on mouse leave
+    $(this).attr('src',$(this).attr('data-orig'));
+  });
+});
